bac024d5fb8fcb4ecebea123f657139e6b3d3eb1
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-05-24 13:54:26 +0530

    feat: Add PaymentController with greeting endpoint and update .gitignore for application.properties

--- server/PaymentService/src/main/java/com/Chatty/PaymentService/controllers/PaymentController.java
+++ server/PaymentService/src/main/java/com/Chatty/PaymentService/controllers/PaymentController.java
@@ -1,4 +1,15 @@
 package com.Chatty.PaymentService.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
 public class PaymentController {
+
+    @GetMapping
+    public String greet(){
+        return "Hello World";
+    }
 }
